ad1eac5c03cb66333d3bab2873ef78f19e9176f1
abrusu-collins <snillochemingson@gmail.com>
2022-02-23 13:20:53 -0800

    Chale!

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  
+    return(
+
+      <div className='App'> 
+      <h1>App</h1>  </div>
+    )
+ 
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import todosComponent from './components/todosComponent';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+     <App />
+    <todosComponent />
+  
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-reportWebVitals();
+
