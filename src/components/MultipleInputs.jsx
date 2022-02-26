fa78c8dae0bef48671e3c416d144199e4427a7be
abrusu-collins <snillochemingson@gmail.com>
2022-02-25 21:04:32 -0800

    Multiple Form Data

--- src/components/MultipleInputs.jsx
+++ src/components/MultipleInputs.jsx
@@ -1,14 +1,28 @@
+import {useState} from 'react';
 function Multiples(){
 
-
+const [Details,setDetails]=useState({name:"Collins Abrusu",age:21})
 
 
     return(
         <form>
+        <label for="name">Name</label>
+        <input type="text" name="name"></input>
         
-        
-        
+
+        <label for="Age">Age</label>
+        <input type="Number" name="Age"></input>
+
+        <button onClick={(e)=>{
+            e.preventDefault();
+            setDetails({name:{}})
+        }}>Submit</button>
+        <p> I am {Details.name}</p>
+        <p> I am {Details.age } years old</p>
+      
         
         </form>
     )
 }
+
+export default Multiples;
