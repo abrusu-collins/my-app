d3e308f6d0d38e7127b4612072346dea500fa5e4
abrusu-collins <snillochemingson@gmail.com>
2022-02-25 18:06:01 -0800

    REACTIVE!

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,26 +1,16 @@
 
 import {useState} from 'react';
 function UseState(){
-const[girl,setGirl]=useState(["Collins","Sedem","Abrusu"]);
-
-let sets=(num)=>{
-        for(let i=0;i<=girl.length-1;i++){
-            let rand=Math.floor(Math.random()*(i+1));
-            let randVal=num[rand];
-             num[rand]=num[i];
-             num[i]=randVal;
-
-        }
-        return num;
-    }
+const[Boy,setBoy]=useState("Collins");
 
 
-// return (
-//     <div>
-//     <h1>I love {girl[1]}</h1>
-//     // <button onClick={}>Switch</button>
-//     </div>
-// );
+
+return (
+    <div>
+    <h1>I love {Boy}</h1>
+    <button onClick={()=>setBoy("Sedem")}>Switch</button>
+    </div>
+);
 
 
 
